cd14154fa95650d8ec600be4101f9d6083f7107c
Mayerli2003 <87994175+Mayerli2003@users.noreply.github.com>
2021-09-07 21:08:38 -0600

    TiendasDonPepe

---
+++ TiendasDonPepe/TiendasDonPepe/Negocios/NClsProductos.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiendasDonPepe.Entidades;
+
+
+
+namespace TiendasDonPepe.Negocios
+
+{
+    
+    class NClsProductos
+
+    {
+        ClsProductos productos = new ClsProductos();
+
+        
+        
+
+        public double totalPagar(ClsProductos productos)
+        {
+            
+            if (productos.Id== 2)
+            {
+                double Respuesta1;
+                
+                Respuesta1 =productos.Monto_precio * 0.15;
+                productos.Total = productos.Monto_precio - Respuesta1;
+                Console.WriteLine("El total es de $" + productos.Total+
+                    "\nDescuento:");
+                return Respuesta1;
+
+            }
+             else if (productos.Id == 4)
+            {
+                double Respuesta1;
+                
+                Respuesta1 = productos.Monto_precio * 0.15;
+                productos.Total = productos.Monto_precio - Respuesta1;
+                Console.WriteLine("El total es de $" + productos.Total+
+                    "\nDescuento:");
+
+                  return Respuesta1;
+                
+
+
+
+            }
+            else
+            {
+                Console.WriteLine("El total a pagar es de $"+ productos.Monto_precio +
+                    "\n Total sin descuento: ");
+                
+                  return   productos.Monto_precio;
+            }
+
+
+        }
+
+    
+    }
+}
